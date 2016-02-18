aac843b2ea4f8c5fa4a6e9fc39fb6e65bf117d79
Tori Hedden <tori.hedden@gmail.com>
2016-02-18 16:12:47 -0500

    Begins to build timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,12 +12,17 @@ $(document).ready(function(){
     });//changes the icon white. needs a delay so the icon doesn't show up right away.
 
 
-    var firstCard = ($(this).children().attr('class'));//logs the class of the div around the icon. I assigned these in week one, to help me position each icon right in the middle of the card. Some FA icons are not the exact same size, so there is a unique class for each and every icon.
+    var firstCard = ($(this).children().attr('class'));
+    //logs the class of the div around the icon. I assigned these in week one, to help me position each icon right in the middle of the card. Some FA icons are not the exact same size, so there is a unique class for each and every icon.
     console.log(firstCard);
-
+    console.log($(this).children().hasClass('diamond'));
+    //will return true for the diamond cards, and false for all else
 
     //if the class of the next thing clicked === the class of $(this).children().attr('class')
 
   });
+
+  $(".timer").append(Date())
+  // setInterval(function(){ console.log(Date()); }, 1000);
 
 });
