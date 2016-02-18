28743ce861934b4262d74e7d84165eee96c9c039
Tori Hedden <tori.hedden@gmail.com>
2016-02-18 15:24:55 -0500

    Adds transform card and icon color change

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
+  'use strict'
 
   $(".card").click(function(){
 
-    var cardChoices = [];
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+    });//rotates the card
 
-    // console.log($(this).attr('class'));
+    $(this).children().css({
+      'color': 'white',
+    });//changes the icon white. needs a delay so the icon doesn't show up right away.
+
+
     var firstCard = ($(this).children().attr('class'));//logs the class of the div around the icon. I assigned these in week one, to help me position each icon right in the middle of the card. Some FA icons are not the exact same size, so there is a unique class for each and every icon.
     console.log(firstCard);
-    cardChoices.push(firstCard);
-    console.log(cardChoices);
 
+
+    //if the class of the next thing clicked === the class of $(this).children().attr('class')
 
   });
 
