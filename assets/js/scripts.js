32926dcfb6265a97f5ec231ebbe51f8b0d92e484
Tori Hedden <tori.hedden@gmail.com>
2016-02-18 22:36:02 -0500

    Adds logic structure to the matching

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   'use strict'
 
+  var firstClass = "";
+
   $(".card").click(function(){
 
     $(this).css({
@@ -11,16 +13,26 @@ $(document).ready(function(){
       'color': 'white',
     });//changes the icon white. needs a delay so the icon doesn't show up right away.
 
+    if (firstClass === ""){
+      firstClass = ($(this).children().attr('class'));
+      console.log("firstClass has been set to " + firstClass )
+    } else {
+      if ($(this).children().hasClass(firstClass)){
+        console.log("You matched them!");//keep both flipped up
+      } else {
+        console.log("No match!");//flip the cards back over
+        console.log("You lost one life!") //(hearts -= 1)
+      }
+      firstClass="";
+    }
 
-    var firstCard = ($(this).children().attr('class'));
+
     //logs the class of the div around the icon. I assigned these in week one, to help me position each icon right in the middle of the card. Some FA icons are not the exact same size, so there is a unique class for each and every icon.
-    console.log(firstCard);
-    console.log($(this).children().hasClass('diamond'));
-    //will return true for the diamond cards, and false for all else
-
     //if the class of the next thing clicked === the class of $(this).children().attr('class')
 
   });
+
+
 
   var secondsPlayed = 0;
 
@@ -41,20 +53,5 @@ $(document).ready(function(){
     var timeFormatted = minutes + ":" + seconds;
     return timeFormatted;
   }
-
-
-  // if (gameStart < 10){
-  //   setInterval(function(){$(".timer").html("00:0"+gameStart); }, 1000);//this replaces the text in the .timer div with the current value of gameTimer
-  // //pad with leading zero?
-  // } else {
-  //   setInterval(function(){$(".timer").html("00:"+gameStart); }, 1000);
-  // }
-  // this worked up until a point... then I got jumps from 242 to 244, 246, 248. Second jumping.
-  // is setInterval imprecise over a longer period of time?
-  // I just checked again around 550 seconds... seems to be incrementing fine...
-
-
-  // console.log(new Date().getSeconds());
-  //will return the seconds according to what actual time it is. no need to round.
 
 });
