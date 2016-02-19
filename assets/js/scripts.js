10c1c58c6d33037ffde357fa42739988012f8dbe
Tori Hedden <tori.hedden@gmail.com>
2016-02-19 09:13:25 -0500

    Adds tracker for life total and matches made

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   'use strict'
 
-  var firstClass = "";
+  var lifeTotal = 6;   //number of lives you start with
+  var numberMatches = 0;//number of matches you have made
+  var firstClass = "";  //will be used to store the class of every first, third, fifth, etc. click
 
   $(".card").click(function(){
 
@@ -19,29 +21,33 @@ $(document).ready(function(){
     } else {
       if ($(this).children().hasClass(firstClass)){
         console.log("You matched them!");//keep both flipped up
-        //matches ++
+        numberMatches ++
         //if (matches = numberOfTiles/2){
           //load winning screen
         //}
       } else {
         console.log("No match!");//flip both cards back over
         console.log("You lost one life!") //(hearts -= 1)
+        lifeTotal = lifeTotal - 1
+        console.log(lifeTotal + " lives remaining");
+        if (lifeTotal === 0){
+          console.log("Out of lives!");
+        }
           //if (lives = 0), load losing screen
       }
       firstClass="";
     }
-
 
     //logs the class of the div around the icon. I assigned these in week one, to help me position each icon right in the middle of the card. Some FA icons are not the exact same size, so there is a unique class for each and every icon.
     //if the class of the next thing clicked === the class of $(this).children().attr('class')
 
   });
 
-
+//Everything below this is just for the timer
   var secondsPlayed = 0;//the page loads and the seconds you have played the game = zero. Because you just started playing the game
 
   setInterval(function(){
-    secondsPlayed += 0.1; //this increases the value of secondsPlayed by one each second
+    secondsPlayed += 0.1; //increases the value of secondsPlayed by one each second, AKA 0.1 seconds 10 times a second
     $(".timer").html(getFormattedTime(secondsPlayed));
   }, 100);
 
